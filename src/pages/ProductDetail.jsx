7a486f0984be0ca35c7232fe0069717fe056d1ea
Night-Venom <ali.farid2020@hotmail.com>
2026-08-26 21:26:48 +0100

    Update ProductDetail.jsx

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,4 +1,4 @@
-const db = globalThis.__B44_DB__ || { auth:{ isAuthenticated: async()=>false, me: async()=>null }, entities:new Proxy({}, { get:()=>({ filter:async()=>[], get:async()=>null, create:async()=>({}), update:async()=>({}), delete:async()=>({}) }) }), integrations:{ Core:{ UploadFile:async()=>({ file_url:'' }) } } };
+supabase.from('products').select('*').order('bestselling_rank');
 
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
