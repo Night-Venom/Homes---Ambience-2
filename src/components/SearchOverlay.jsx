b192526e32be69ecb7d37cff12600c95f13df38e
Night-Venom <ali.farid2020@hotmail.com>
2026-08-26 21:27:36 +0100

    Update SearchOverlay.jsx

--- src/components/SearchOverlay.jsx
+++ src/components/SearchOverlay.jsx
@@ -1,4 +1,4 @@
-const db = globalThis.__B44_DB__ || { auth:{ isAuthenticated: async()=>false, me: async()=>null }, entities:new Proxy({}, { get:()=>({ filter:async()=>[], get:async()=>null, create:async()=>({}), update:async()=>({}), delete:async()=>({}) }) }), integrations:{ Core:{ UploadFile:async()=>({ file_url:'' }) } } };
+supabase.from('products').select('*').eq('id', id).maybeSingle();
 
 import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
